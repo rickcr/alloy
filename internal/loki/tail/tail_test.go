01cee2e72e821e486219aa7394b75cd855500c27
Paulin Todev <paulin.todev@gmail.com>
2025-10-27 22:16:50 +0000

    logs

--- internal/loki/tail/tail_test.go
+++ internal/loki/tail/tail_test.go
@@ -337,6 +337,7 @@ func reOpen(t *testing.T, poll bool) {
 		delay = 100 * time.Millisecond
 	}
 	tailTest := NewTailTest(name, t)
+	t.Logf("creating file")
 	tailTest.CreateFile("test.txt", "hello\nworld\n")
 	tail := tailTest.StartTail(
 		"test.txt",
@@ -347,26 +348,32 @@ func reOpen(t *testing.T, poll bool) {
 	if poll {
 		// deletion must trigger reopen
 		<-time.After(delay)
+		t.Logf("removing file")
 		tailTest.RemoveFile("test.txt")
 		<-time.After(delay)
+		t.Logf("creating file")
 		tailTest.CreateFile("test.txt", "more\ndata\n")
 	} else {
 		// In inotify mode, fsnotify is currently unable to deliver notifications
 		// about deletion of open files, so we are not testing file deletion.
 		// (see https://github.com/fsnotify/fsnotify/issues/194 for details).
 		<-time.After(delay)
+		t.Logf("appending to file")
 		tailTest.AppendToFile("test.txt", "more\ndata\n")
 	}
 
 	// rename must trigger reopen
 	<-time.After(delay)
+	t.Logf("renaming file")
 	tailTest.RenameFile("test.txt", "test.txt.rotated")
 	<-time.After(delay)
+	t.Logf("creating file")
 	tailTest.CreateFile("test.txt", "endofworld\n")
 
 	// Delete after a reasonable delay, to give tail sufficient time
 	// to read all lines.
 	<-time.After(delay)
+	t.Logf("removing file")
 	tailTest.RemoveFile("test.txt")
 	<-time.After(delay)
 
